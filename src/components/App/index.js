3e2c17533072b43672e0e9d8174c338f5a3b216c
Murray2015 <murrayhoggett@gmail.com>
2020-03-09 16:44:56 +0000

    Working first map

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import css from "./App.module.css";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 function App() {
+  const bhamPosition = [52.4862, -1.8904];
+  const map = (
+    <Map center={bhamPosition} zoom={8}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </Map>
+  );
   return (
     <div className={css.container}>
-      <h1>Happy Hacking!</h1>
+      {map}
+      <h1>Happy mapping!</h1>
     </div>
   );
 }
