9e1237019b995fb5f3e498b8cd39f066ed809cfa
Murray2015 <murrayhoggett@gmail.com>
2020-03-11 10:25:53 +0000

    Added local authority boundaries attempt 2 (ignore last commit)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import css from './App.module.css';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import ReactLeafletSearch from 'react-leaflet-search';
-import L from 'leaflet';
-import Form from '../Form';
+import React, { useState } from "react";
+import css from "./App.module.css";
+import { Map, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
+import ReactLeafletSearch from "react-leaflet-search";
+import L from "leaflet";
+import Form from "../Form";
 import {
   TwitterTimelineEmbed,
   TwitterHashtagButton
-} from 'react-twitter-embed';
+} from "react-twitter-embed";
+import bhamPoly from "../../const_wgs84";
 
 // Set global vars
 let allowTreeAdd = false;
@@ -17,95 +18,95 @@ const dummyData = [
   {
     lat: 52.862,
     lon: -1.904,
-    species: 'larch',
-    status: 'planted',
+    species: "larch",
+    status: "planted",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.62,
     lon: -1.04,
-    species: 'elm',
-    status: 'requested',
+    species: "elm",
+    status: "requested",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.2,
     lon: -1.4,
-    species: 'ash',
-    status: 'planted',
+    species: "ash",
+    status: "planted",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.4862,
     lon: -1.8904,
-    species: 'birch',
-    status: 'requested',
+    species: "birch",
+    status: "requested",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.862,
     lon: -1.8904,
-    species: 'larch',
-    status: 'planted',
+    species: "larch",
+    status: "planted",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.62,
     lon: -1.8904,
-    species: 'elm',
-    status: 'requested',
+    species: "elm",
+    status: "requested",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.2,
     lon: -1.8904,
-    species: 'ash',
-    status: 'requested',
+    species: "ash",
+    status: "requested",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.4862,
     lon: -1.8904,
-    species: 'birch',
-    status: 'planted',
+    species: "birch",
+    status: "planted",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.4862,
     lon: -1.904,
-    species: 'larch',
-    status: 'planted',
+    species: "larch",
+    status: "planted",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.4862,
     lon: -1.04,
-    species: 'elm',
-    status: 'requested',
+    species: "elm",
+    status: "requested",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   },
   {
     lat: 52.4862,
     lon: -1.4,
-    species: 'ash',
-    status: 'planted',
+    species: "ash",
+    status: "planted",
     photo:
-      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
   }
 ];
 
 const redTreeMarker = new L.icon({
-  iconUrl: require('../../img/icon_red.png'),
+  iconUrl: require("../../img/icon_red.png"),
   iconAnchor: [0, 0],
   popupAnchor: [0, 0],
   shadowUrl: null,
@@ -115,7 +116,7 @@ const redTreeMarker = new L.icon({
 });
 
 const greenTreeMarker = new L.icon({
-  iconUrl: require('../../img/icon_green.png'),
+  iconUrl: require("../../img/icon_green.png"),
   iconAnchor: [0, 0],
   popupAnchor: [0, 0],
   shadowUrl: null,
@@ -149,10 +150,10 @@ function App() {
     const newTree = {
       lat: lat,
       lon: lng,
-      species: 'larch',
-      status: 'planted',
+      species: "larch",
+      status: "planted",
       photo:
-        'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg'
+        "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"
     };
     allowTreeAdd && setTrees([...trees, newTree]);
   }
@@ -168,7 +169,7 @@ function App() {
       <ReactLeafletSearch
         position="topleft"
         provider="OpenStreetMap"
-        providerOptions={{ region: 'gb' }}
+        providerOptions={{ region: "gb" }}
         inputPlaceholder="Search by postcode or area name"
         closeResultsOnClick={true}
         showPopup={true}
@@ -177,10 +178,14 @@ function App() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <GeoJSON
+        data={bhamPoly}
+        style={{ color: "darkgreen", fillColor: "lightgreen", weight: 1 }}
+      />
       {trees.map(x => {
         return (
           <Marker
-            icon={x.status === 'planted' ? greenTreeMarker : redTreeMarker}
+            icon={x.status === "planted" ? greenTreeMarker : redTreeMarker}
             position={[x.lat, x.lon]}
             key={x.status + x.lat + x.lon + x.species}
           >
@@ -214,11 +219,11 @@ function App() {
         <TwitterTimelineEmbed
           sourceType="profile"
           screenName="WestMids_CA"
-          options={{ width: '600px', height: '900px' }}
+          options={{ width: "600px", height: "900px" }}
         />
       </div>
       <div className={css.twitterHashTagButton}>
-        <TwitterHashtagButton tag={'NeedsATree'} />
+        <TwitterHashtagButton tag={"NeedsATree"} />
       </div>
       <footer>
         <a href="https://www.wmca.org.uk/careers?_ga=2.233332958.1505638702.1583852010-1790124967.1583750647">
