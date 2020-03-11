646351bf950f1b94f77d0fe650a8ce7658a1b9f6
cherylasif <cherylsoniaasif@yahoo.co.uk>
2020-03-11 12:04:16 +0000

    Added radio buttons to form

--- src/components/FormPrivacyOptions/index.js
+++ src/components/FormPrivacyOptions/index.js
@@ -3,10 +3,39 @@
 // -- STRETCH: individual controls for each part of data
 // -- Need to not show phone number/email by default! These need to not show by default regardless of this setting.
 
-import React from 'react';
+import React, { useState } from "react";
 
 function FormPrivacyOptions() {
-  return <></>;
+  const [change, setChange] = useState(0);
+  function handleOptionChange(changeEvent) {
+    setChange(changeEvent.target.value);
+  }
+  return (
+    <div className="radio">
+      Click if you wish you Name and Tree information to be posted
+      <input
+        type="radio"
+        name="radio-btn"
+        value="option1"
+        onChange={handleOptionChange}
+      />
+      Click if you wish your Name, Tree and Dedication information to be posted
+      <input
+        type="radio"
+        name="radio-btn"
+        value="option2"
+        onChange={handleOptionChange}
+      />
+      Click if you wish to remain Anonymous
+      <input
+        type="radio"
+        name="radio-btn"
+        value="option2"
+        onChange={handleOptionChange}
+      />
+    </div>
+  );
 }
+// Please test when tying in with the map. Not throwing any errors yet. May do later
 
 export default FormPrivacyOptions;
