2c069adc98a7dd4d2afd03de85382782d1568703
cherylasif <cherylsoniaasif@yahoo.co.uk>
2020-03-12 13:37:19 +0000

    Give counter classname

--- src/components/Impact/index.js
+++ src/components/Impact/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import css from './Impact.module.css';
 
 // source for tree co2 absorption: http://urbanforestrynetwork.org/benefits/air%20quality.htm
 function Impact({ count }) {
   return (
     <div className={css.impact}>
-      <p id="tree-count">{count} Trees planted.</p>
-      <p>Approx {5.8967 * count}kg CO2 absorbed per year.</p>
+      <p>
+        <span className={css.treeCount}>{count}</span>Trees planted.
+      </p>
+      <p>
+        Approx <span className={css.emissionCount}>{5.8967 * count}</span>kg CO2
+        absorbed per year.
+      </p>
     </div>
   );
 }
-
 export default Impact;
